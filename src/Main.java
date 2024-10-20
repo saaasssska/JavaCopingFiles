474a059886f15795e49970b37a8682596a993ee3
saaasssska <fedyev981@gmail.com>
2024-10-20 19:07:56 +0300

    no interface

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import java.nio.file.*;
 
 public class Main {
     static Scanner s = new Scanner(System.in);
+    static int num;
     static String pathIn = "";
+    static String pathOut = "";
 
     public static Boolean checking() {
         String check;
@@ -14,33 +16,18 @@ public class Main {
         return check.equalsIgnoreCase("да");
     }
 
-    public static String checkExcept() throws IOException {
-        System.out.println("Введите путь файла");
-        pathIn = s.nextLine();
-        try {
-            Path fis = Paths.get(
-                    "C:\\Users\\fedye\\OneDrive\\Рабочий стол\\input5.txt");
-        }
-        catch (Exception exception) {
-            System.out.println("Файла не существует");
-            return null;
-        }
-        return pathIn;
+    public static boolean fileExists(String path) {
+        return Files.exists(Paths.get(path));
     }
-    public static void ioCoping() throws IOException {
+
+    public static void ioCoping(String pathIn, String pathOut) throws IOException {
         FileInputStream fis = null;
         FileOutputStream fos = null;
 
-        if (!checking()) {
-            return;
-        }
-
         try {
-            fis = new FileInputStream(
-                    "C:\\Users\\fedye\\OneDrive\\Рабочий стол\\input.txt");
+            fis = new FileInputStream(pathIn);
 
-            fos = new FileOutputStream(
-                    "C:\\Users\\fedye\\OneDrive\\Рабочий стол\\output.txt");
+            fos = new FileOutputStream(pathOut);
 
             byte[] buffer = new byte[1024];
             int lengthRead;
@@ -49,6 +36,9 @@ public class Main {
                 fos.flush();
             }
             System.out.println("Файл успешно скопирован");
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
         finally {
             if (fis != null) {
@@ -59,46 +49,65 @@ public class Main {
             }
         }
     }
+    public static void nioCopingChanels(String pathIn, String pathOut) throws IOException {
+        FileChannel fis = new FileInputStream(pathIn).getChannel();
+        FileChannel fos = new FileOutputStream(pathOut).getChannel();
 
-    public static void nioCopingChanels() throws IOException {
-        if (!checking()) {
-            return;
+        try {
+            fos.transferFrom(fis, 0, fis.size());
+            System.out.println("Файл успешно скопирован");
         }
-        FileChannel fis = new FileInputStream(
-                "C:\\Users\\fedye\\OneDrive\\Рабочий стол\\input.txt").getChannel();
-
-        FileChannel fos = new FileOutputStream(
-                "C:\\Users\\fedye\\OneDrive\\Рабочий стол\\output2.txt").getChannel();
-
-        fos.transferFrom(fis, 0, fis.size());
-        System.out.println("Файл успешно скопирован");
-        fis.close();
-        fos.close();
+        catch (IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        finally {
+            fis.close();
+            fos.close();
+        }
     }
 
-    public static void nio2Coping() throws IOException {
-        if (!checking()) {
-            return;
+    public static void nio2Coping(String pathIn, String pathOut) throws IOException {
+        try {
+            Files.copy(Paths.get(pathIn), Paths.get(pathOut), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Файл скопирован");
+        } catch (IOException e) {
+            System.out.println("Ошибка копирования файла: " + e.getMessage());
         }
-
-        pathIn = checkExcept();
-        if (pathIn == null) {
-            return;
-        }
-
-        Path fis = Paths.get(pathIn);
-
-        Path fos = Paths.get(
-                "C:\\Users\\fedye\\OneDrive\\Рабочий стол\\output5.txt");
-        Files.copy(fis, fos, StandardCopyOption.REPLACE_EXISTING);
-        System.out.println("Файл скопирован");
     }
 
     public static void main(String[] args)
             throws IOException {
-        //ioCoping();
-        //nioCopingChanels();
-        nio2Coping();
+        while (true) {
+            System.out.println("Выберите способ копирования файла");
+            System.out.println("1 - пакет io");
+            System.out.println("2 - пакет nio");
+            System.out.println("3 - пакет nio2");
+            num = s.nextInt();
+            s.nextLine();
+            if (!(num == 1 || num == 2 || num == 3)) {
+                break;
+            }
+            System.out.println("Введите путь файла");
+            pathIn = s.nextLine();
+            if (!fileExists(pathIn)) {
+                System.out.println("Файл для копирования не существует: " + pathIn);
+                return;
+            }
+
+            System.out.println("Введите путь файла, куда копировать");
+            pathOut = s.nextLine();
+            if (fileExists(pathOut)) {
+                if (!checking()) {
+                    continue;
+                }
+            }
+
+            switch (num) {
+                case 1: ioCoping(pathIn, pathOut); break;
+                case 2: nioCopingChanels(pathIn, pathOut); break;
+                case 3: nio2Coping(pathIn, pathOut); break;
+            }
+        }
     }
 }
 // C:\Users\fedye\OneDrive\Рабочий стол\input.txt
